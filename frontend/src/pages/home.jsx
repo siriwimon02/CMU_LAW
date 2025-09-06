1e701c199d41b38309150ad9e8a58a7d26305b90
2pleum <kantida.mails@gmail.com>
2025-09-06 20:36:39 +0700

    dev frontend home page structure

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -4,7 +4,26 @@ import { useNavigate } from 'react-router-dom';
 
 function Home() {
     return (
-        <div>home</div>
+        <div>
+            <img
+                src="/images/Logo.svg"
+                alt="Logo"
+                className="fixed top-6 left-25 w-[180px] h-[69px] object-contain z-50"
+            />
+            <div className="fixed top-10 right-20 object-contain z-50">
+                <h1 className="text-3xl font-bold text-center">Chiang Mai University Project Authorization</h1>
+            </div>
+            <div className="max-w-[1440px] w-full min-h-[590px] mx-auto bg-[url('/images/home_background.jpg')] bg-no-repeat bg-center bg-cover mt-30">
+                <div className="p-10 text-white">
+                    <h1 className="text-5xl font-bold text-center mt-20">ระบบบริหารจัดการหนังสือมอบอำนาจออนไลน์</h1>
+                </div>
+                <div className="text-white">
+                    <h1 className="text-2xl font-bold text-center">จัดการคำขอมอบอำนาจ ติดตามสถานะ และอนุมัติเอกสารได้อย่างมีประสิทธิภาพ</h1>
+                </div>
+            </div>
+        </div>
+
+
     )
 };
 
