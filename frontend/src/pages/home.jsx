d44345fd8b7b8103a3bb09d160f0474e69984596
2pleum <kantida.mails@gmail.com>
2025-09-06 23:10:30 +0700

    manage header homepage

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 function Home() {
     return (
         <div>
-            <div className="fixed top-6 left-25 flex items-center gap-4 z-50">
+            <div className="fixed top-6 inset-x-0 mx-auto flex items-center gap-3 z-50 w-fit px-4">
                 <img
                     src="/images/Logo.svg"
                     alt="Logo"
@@ -18,13 +18,11 @@ function Home() {
 
             <div className="max-w-[1440px] w-full min-h-[590px] mx-auto bg-[url('/images/home_background.jpg')] bg-no-repeat bg-center bg-cover mt-30">
                 <div className="p-10 text-white">
-                    <h1 className="text-5xl font-bold text-center mt-20">ระบบบริหารจัดการหนังสือมอบอำนาจออนไลน์</h1>
+                    <h1 className="text-5xl font-bold text-center mt-10">ระบบบริหารจัดการหนังสือมอบอำนาจออนไลน์</h1>
+                    <h2 className="text-2xl text-center mt-10">จัดการคำขอมอบอำนาจ ติดตามสถานะ และอนุมัติเอกสารได้อย่างมีประสิทธิภาพ</h2>
                 </div>
-                <div className="text-white">
-                    <h1 className="text-2xl text-center">จัดการคำขอมอบอำนาจ ติดตามสถานะ และอนุมัติเอกสารได้อย่างมีประสิทธิภาพ</h1>
-                </div>
-                <div className="flex justify-center mt-50">
-                    <button className="w-[493px] h-[100px] rounded-[20px] text-[#66009F] text-3xl text-bold flex items-center justify-center bg-white 
+                <div className="flex justify-center items-end h-75">
+                    <button className="w-[493px] h-[80px] rounded-[20px] text-[#66009F] text-3xl text-bold flex items-center justify-center bg-white 
                     font-medium shadow hover:bg-[#66009F]  hover:text-white transition-colors duration-300">
                         Login With CMU Account
                     </button>
