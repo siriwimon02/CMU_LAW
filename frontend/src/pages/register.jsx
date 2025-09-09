4bb7562ef477d79d10cba35290533560eac0b8d2
Cheekkii <gamsasivimon@gmail.com>
2025-09-09 13:15:09 +0700

    setting font in frontend

--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -1,14 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Register() {
   const [form, setForm] = useState({
     email: '',
-    password: '',
     firstname: '',
     lastname: '',
+    department: '',
+    role_id: ''
   });
+
+  const [roleOfUser, setroleOfuser] = useState(null);
+
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
+
+
+  useEffect( () => {
+    async function getRoleUser() {
+      const res1 = await fetch('http://localhost:3001/api/roleofuser', {
+        
+      })
+    }
+  });
+
+
 
   const handleChange = e => {
     setForm(prev => ({ ...prev, [e.target.name]: e.target.value }));
@@ -38,13 +53,18 @@ export default function Register() {
     setLoading(false);
   };
 
+
+  
+
   return (
     <form onSubmit={handleRegister}>
       <h2>Register</h2>
-      <input type="email" name="email" value={form.email} onChange={handleChange} required placeholder="Email" />
-      <input type="password" name="password" value={form.password} onChange={handleChange} required placeholder="Password" />
-      <input type="text" name="firstname" value={form.firstname} onChange={handleChange} required placeholder="First Name" />
+      <h1 className="font-kanit text-3xl">สวัสดี Tailwind</h1>
+      <input type="email" name="email" value={form.email} onChange={handleChange} required placeholder="Email CMU" />
+      <input type="text" name="firstname" value={form.firstname} onChange={handleChange} required placeholder="First name" />
       <input type="text" name="lastname" value={form.lastname} onChange={handleChange} required placeholder="Last Name" />
+      <input type="text" name="department" value={form.department} onChange={handleChange} required placeholder="Department" />
+
       {error && <p style={{ color: 'red' }}>{error}</p>}
       <button disabled={loading}>{loading ? 'Loading...' : 'Register'}</button>
     </form>
